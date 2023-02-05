26c45fbdab8a6d16d30d0c172665981d47ddcbee
bowber <46023302+bowber@users.noreply.github.com>
2023-02-06 00:56:25 +0700

    Ignore backend/local.cmd

--- backend/local.cmd
+++
@@ -1,7 +0,0 @@
-set TWILIO_ACCOUNT_SID=your-twilio-account-sid
-set TWILIO_AUTH_TOKEN=your-twilio-auth-token
-set TWILIO_MESSAGING_SID=your-twilio-messaging-sid
-set TWILIO_SENDER_PHONE_NUMBER=your-twilio-phone-number
-set FIRESTORE_EMULATOR_HOST=localhost:8081
-set GCLOUD_PROJECT=your-project-id
-npm run build && npm run local
