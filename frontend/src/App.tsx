873e1fbcaf4fba7bef374e9168bbf31cda3ce405
bowber <46023302+bowber@users.noreply.github.com>
2023-02-05 18:44:42 +0700

    Change Dockerfile runner from node to pm2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,41 @@
 import { useState } from 'react';
 import './App.css';
 
+const auth_host = process.env.REACT_APP_AUTH_HOST;
+
 function App() {
   const [phoneNumber, changePhoneNumber] = useState("")
   const [accessCode, changeAccessCode] = useState("")
 
-  const sendAccessCode = (phoneNum: string) => {
+  const sendAccessCode = async (phoneNum: string) => {
     console.log("Send Access Code " + phoneNum);
+    const res = await fetch(auth_host + "/login/access-code", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        phoneNumber: phoneNum
+      })
+    })
+    alert(res.statusText)
   }
+
+
   const signIn = (phoneNum: string, code: string) => {
     console.log("SignIn " + phoneNum + " " + code);
+    fetch(auth_host + "/login/validate", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        phoneNumber: phoneNum,
+        accessCode: code
+      })
+    })
+    .then(res => alert(res.statusText))
+    .catch(err => alert(err))
 
   }
   return (
